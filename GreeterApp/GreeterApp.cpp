86c13c493115be138a17ae4ebbc887fe5abcb7b4
flam1ngo <kris.s.03@mail.ru>
2024-03-26 20:28:21 +0300

    Initial commit

---
+++ GreeterApp/GreeterApp.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include "greeter.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Greeter greeter;
+    std::string input;
+    std::cout << "Введите имя: ";
+    std::cin >> input;
+
+    std::string greeting = greeter.greet(input);
+
+    std::cout << greeting << std::endl;
+
+    return 0;
+}
+
---
+++ GreeterLibraryStatic/greeter.cpp
@@ -0,0 +1,5 @@
+#include "greeter.h"
+
+std::string Greeter::greet(std::string& input) {
+	return "Здравствуйте, " + input + "!";
+}
